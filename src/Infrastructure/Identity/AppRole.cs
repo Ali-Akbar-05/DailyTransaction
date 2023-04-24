3353f2dd9aeda3b301a171d1f232be58d544d6e1
akbarbd <aliakbar.a05@gmail.com>
2023-04-24 21:22:35 +0600

    Identity

---
+++ src/Infrastructure/Identity/AppRole.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity;
+
+public class AppRole : IdentityRole<int>
+{
+    public AppRole(string roleName):base(roleName)
+    {
+        
+    }
+    public bool IsSuperAdmin { get; set; }
+    public DateTime CreateionDate { get; set; }
+    public DateTime? LastModifiedDate { get; set; }
+}
